afc2816122778bc03efdadbf9f9908ae2de60afe
suwonkun <zhiyinzhongze@gmail.com>
2023-11-04 15:02:53 +0900

    マイグレーションの修正

--- workspace/src/main/resources/db/migration/V1.0.0__create_users.sql
+++ workspace/src/main/resources/db/migration/V1.0.0__create_users.sql
@@ -1,5 +1,7 @@
-CREATE TABLE user (
+CREATE TABLE users (
   id bigint NOT NULL,
   name varchar(32) NOT NULL,
+  email varchar(32) NOT NULL,
+  password varchar(32) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
